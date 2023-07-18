3d0f4623b494aeef4ab0ea50df136bff0e852700
imjunhui <yim3370@gmail.com>
2023-07-18 22:03:11 +0900

    Spring Cloud MSA
    - api gateway use filter with java code

--- user-service/src/main/java/com/msa/userservice/TestService.java
+++ user-service/src/main/java/com/msa/userservice/TestService.java
@@ -1,9 +1,12 @@
 package com.msa.userservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/")
+@Slf4j
 public class TestService {
     @GetMapping("first-service/welcome")
     public String welcome1() {
@@ -13,5 +16,17 @@ public class TestService {
     @GetMapping("second-service/welcome")
     public String welcome2() {
         return "welcome to second service";
+    }
+
+    @GetMapping("first-service/message")
+    public String message1(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "first service message";
+    }
+
+    @GetMapping("second-service/message")
+    public String message2(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return "second service message";
     }
 }
