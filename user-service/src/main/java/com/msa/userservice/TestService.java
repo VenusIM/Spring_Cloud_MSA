0ae8cb0ac6bd72474ae78e7c5df70ccf58534784
imjunhui <yim3370@gmail.com>
2023-07-20 22:14:15 +0900

    Spring Cloud MSA Gateway LoadBalance
    - lb://"eureka-service-name"

--- user-service/src/main/java/com/msa/userservice/TestService.java
+++ user-service/src/main/java/com/msa/userservice/TestService.java
@@ -1,13 +1,21 @@
 package com.msa.userservice;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController("/")
 @Slf4j
+@RequiredArgsConstructor
 public class TestService {
+
+    private final Environment environment;
+
     @GetMapping("first-service/welcome")
     public String welcome1() {
         return "welcome to first service";
@@ -31,8 +39,9 @@ public class TestService {
     }
 
     @GetMapping("first-service/check")
-    public String check1() {
-        return "first service check";
+    public String check1(HttpServletRequest request) {
+        log.info("Server port = {}", request.getServerPort());
+        return String.format("Environment Server port %s", environment.getProperty("local.server.port"));
     }
 
     @GetMapping("second-service/check")
